953e801bd1f10842cd893e584f731478b12aef6f
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-02-28 16:40:05 -0500

    Update script.js
    Added fe prefixes to avoid future plugin conflicts

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@
 document.addEventListener('DOMContentLoaded', function() {
   var feloader = document.querySelector('.fe-loader-overlay');
 // Show loader when needed, but just in case get it outta here after 3 seconds
-  function showLoader() {
+  function feshowLoader() {
     feloader.style.display = 'flex';
     setTimeout(hideLoader, 3000);
   }
 // Hide loader when needed
-  function hideLoader() {
+  function fehideLoader() {
     feloader.style.display = 'none';
   }
 // For an app experience, show loader on link clicks
   document.querySelectorAll('a').forEach(function(link) {
     link.addEventListener('click', function(event) {
-      showLoader();
+      feshowLoader();
     });
   });
 // Actually do the hiding
-  window.addEventListener('load', hideLoader);
+  window.addEventListener('load', fehideLoader);
 
 // Check if jQuery is available before setting up AJAX handlers
   if (window.jQuery) {
 // Using jQuery for AJAX request handling for an app-like experience - remove this part if it's too bothersome for you
     jQuery(document).on('ajaxSend', function(e, xhr, options) {
       if (!options.data || (options.data && !options.data.includes('heartbeat'))) {
-        showLoader();
+        feshowLoader();
       }
     }).on('ajaxComplete', function(e, xhr, options) {
-      hideLoader();
+      fehideLoader();
     });
   } else {
 // jQuery is not available - let the dev know in the console
