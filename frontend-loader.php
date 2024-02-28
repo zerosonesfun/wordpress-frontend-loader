f313ace959e1cf491acc55d41301697c55841de1
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-02-28 16:20:16 -0500

    Update frontend-loader.php
    Update CSS version.

--- frontend-loader.php
+++ frontend-loader.php
@@ -22,7 +22,7 @@ if ( ! function_exists( 'add_action' ) ) {
 // Prefix functions with "feload"
 function feload_custom_enqueue_scripts_and_styles() {
     wp_enqueue_script( 'feload-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.2', true );
-    wp_enqueue_style( 'feload-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.2' );
+    wp_enqueue_style( 'feload-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.3' );
 }
 add_action( 'wp_enqueue_scripts', 'feload_custom_enqueue_scripts_and_styles' );
 
