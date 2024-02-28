5bffd2962e767801e787d4a6116103b84c481dc0
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-02-28 16:38:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
-// Let us begin. Once the DOM is loaded, look for the overlay div.
-
+// Target the fe-loader-overlay div which the php file injects
 document.addEventListener('DOMContentLoaded', function() {
   var feloader = document.querySelector('.fe-loader-overlay');
-  
-// When showing the loader, do it like this.
-  
+// Show loader when needed, but just in case get it outta here after 3 seconds
   function showLoader() {
     feloader.style.display = 'flex';
-    setTimeout(hideLoader, 3000); // Fallback to hide loader after 3 seconds just to ensure that no visitor ever has too poor of an experience due to an outlying/odd situation.
+    setTimeout(hideLoader, 3000);
   }
-
-// Do this to hide the loader.
-  
+// Hide loader when needed
   function hideLoader() {
     feloader.style.display = 'none';
   }
-
-// For an app experience, show the loader on link clicks. One can change the selector. This would be a nice setting if a settings page is ever added.
-  
+// For an app experience, show loader on link clicks
   document.querySelectorAll('a').forEach(function(link) {
     link.addEventListener('click', function(event) {
       showLoader();
     });
   });
-
+// Actually do the hiding
   window.addEventListener('load', hideLoader);
 
-// Check AJAX requests using jQuery because WordPress loads jQuery by default so why not utilize it?
-  
-  jQuery(document).on('ajaxSend', function(e, xhr, options) {
-    // Ignore WP hearbeat - that would cause the spinner to show every so many seconds/minutes.
-    if (!options.data || (options.data && !options.data.includes('heartbeat'))) {
-      showLoader();
-    }
-  }).on('ajaxComplete', function(e, xhr, options) {
-    hideLoader();
-  });
+// Check if jQuery is available before setting up AJAX handlers
+  if (window.jQuery) {
+// Using jQuery for AJAX request handling for an app-like experience - remove this part if it's too bothersome for you
+    jQuery(document).on('ajaxSend', function(e, xhr, options) {
+      if (!options.data || (options.data && !options.data.includes('heartbeat'))) {
+        showLoader();
+      }
+    }).on('ajaxComplete', function(e, xhr, options) {
+      hideLoader();
+    });
+  } else {
+// jQuery is not available - let the dev know in the console
+    console.warn("jQuery is not loaded, AJAX request handling for loader display is disabled.");
+  }
 });
