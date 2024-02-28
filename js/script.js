f9da84013082fc2c72c0233ed10675f3aada8b74
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-02-28 16:34:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // Let us begin. Once the DOM is loaded, look for the overlay div.
 
 document.addEventListener('DOMContentLoaded', function() {
-  var loader = document.querySelector('.fe-loader-overlay');
+  var feloader = document.querySelector('.fe-loader-overlay');
   
 // When showing the loader, do it like this.
   
   function showLoader() {
-    fe-loader.style.display = 'flex';
+    feloader.style.display = 'flex';
     setTimeout(hideLoader, 3000); // Fallback to hide loader after 3 seconds just to ensure that no visitor ever has too poor of an experience due to an outlying/odd situation.
   }
 
 // Do this to hide the loader.
   
   function hideLoader() {
-    fe-loader.style.display = 'none';
+    feloader.style.display = 'none';
   }
 
 // For an app experience, show the loader on link clicks. One can change the selector. This would be a nice setting if a settings page is ever added.
