8e56d911b57cfb553ef030614a82ed2a71df5c3d
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-03-04 06:30:26 -0500

    Update frontend-loader.php

--- frontend-loader.php
+++ frontend-loader.php
@@ -21,7 +21,7 @@ if ( ! function_exists( 'add_action' ) ) {
 
 // Prefix functions with "feload"
 function feload_custom_enqueue_scripts_and_styles() {
-    wp_enqueue_script( 'load-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.7', true );
+    wp_enqueue_script( 'load-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.8', true );
     wp_enqueue_style( 'load-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.7' );
 }
 add_action( 'wp_enqueue_scripts', 'feload_custom_enqueue_scripts_and_styles' );
