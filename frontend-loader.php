a5b2391833c6ec540387a620773e7ddeb4e43870
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2024-03-04 07:52:10 -0500

    Update frontend-loader.php

--- frontend-loader.php
+++ frontend-loader.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Frontend Loader
 Description: A minimal loading spinner for WordPress that just works from initial click to page load to ajax requests.
-Version: 1.3
+Version: 1.3.5
 Author: Billy Wilcosky
 Author URI: https://wilcosky.com/skywolf
 License: GPL v2 or later
@@ -23,8 +23,8 @@ if ( ! function_exists( 'add_action' ) ) {
 
 // Enqueue scripts and styles
 function feload_custom_enqueue_scripts_and_styles() {
-    wp_enqueue_script( 'load-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.4', true );
-    wp_enqueue_style( 'load-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.3' );
+    wp_enqueue_script( 'load-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.3.6', true );
+    wp_enqueue_style( 'load-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.3.6' );
 
     // Retrieve settings or set default values
     $ignore_click_selectors = get_option('feload_ignore_click_selectors', '.cld-like-trigger, .wp_ulike_btn');
